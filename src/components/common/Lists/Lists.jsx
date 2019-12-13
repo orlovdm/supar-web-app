8fdc09aa3464f39f3c8bdcacb7cb6d293dba50ff
orlovdm <32155488+orlovdm@users.noreply.github.com>
2019-12-13 15:20:50 +0300

    Add Modules and Elements Lists

--- src/components/common/Lists/Lists.jsx
+++ src/components/common/Lists/Lists.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MachinesAPI, ServiceManAPI } from "./../../../Api/api";
+import { MachinesAPI, ServiceManAPI, ModulesAPI, ElementsAPI } from "./../../../Api/api";
 import {
     Select,
 } from '@storaensods/seeds-react';
@@ -25,6 +25,72 @@ export const ListMachines = props => {
         <div className={'se-form-group'}>
             <div className={'se-input-container'}>
                 <label>Machine:</label>
+                <Select options={data}
+                    onChange={value => input.onChange(value)}
+                    onBlur={() => input.onBlur(input.value)}
+                    value={input.value}
+                    {...restProps}
+                />
+            </div>
+            <small className="se-form-help se-form-help--invalid">{hasError && error}</small>
+        </div>
+    );
+}
+
+export const ListModules = props => {
+
+    const { input, meta: { touched, error }, machine, ...restProps } = props;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await ModulesAPI.getModules(machine);
+            result.status === 200
+                ? setData(result.data.items.map(x => ({ value: x.id, label: x.name })))
+                : console.warn(`${result.status} - ${result.statusText}`);
+        }
+        fetchData();
+    }, []);
+
+    const hasError = touched && error;
+
+    return (
+        <div className={'se-form-group'}>
+            <div className={'se-input-container'}>
+                <label>Module:</label>
+                <Select options={data}
+                    onChange={value => input.onChange(value)}
+                    onBlur={() => input.onBlur(input.value)}
+                    value={input.value}
+                    {...restProps}
+                />
+            </div>
+            <small className="se-form-help se-form-help--invalid">{hasError && error}</small>
+        </div>
+    );
+}
+
+export const ListElements = props => {
+
+    const { input, meta: { touched, error }, module, ...restProps } = props;
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await ElementsAPI.getElements(module);
+            result.status === 200
+                ? setData(result.data.items.map(x => ({ value: x.id, label: x.name })))
+                : console.warn(`${result.status} - ${result.statusText}`);
+        }
+        fetchData();
+    }, []);
+
+    const hasError = touched && error;
+
+    return (
+        <div className={'se-form-group'}>
+            <div className={'se-input-container'}>
+                <label>Element:</label>
                 <Select options={data}
                     onChange={value => input.onChange(value)}
                     onBlur={() => input.onBlur(input.value)}
